d94259d4a2113f5294a89e2cd95cda6b5186d048
Cactus5835 <xzdxrz666@gmail.com>
2026-05-11 23:10:45 +1000

    feat: add nvim-tree and nvim-spectre
    - Add nvim-tree file explorer with compat-nvim-0.9 tag for Nvim 0.9
      support; toggle via <leader>e
    - Add nvim-spectre find-and-replace panel; toggle via <leader>S, search
      word via <leader>sw
    - Disable netrw at startup to avoid conflict with nvim-tree

---
+++ lua/plugins/spectre.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "nvim-pack/nvim-spectre",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>S", "<cmd>lua require('spectre').toggle()<CR>", desc = "Toggle Spectre" },
+      { "<leader>sw", "<cmd>lua require('spectre').open_visual({ select_word = true })<CR>", desc = "Search current word" },
+    },
+    config = function()
+      require("spectre").setup({ color_devicons = true })
+    end,
+  },
+}
---
+++ lua/plugins/tree.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "nvim-tree/nvim-tree.lua",
+    tag = "compat-nvim-0.9",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+      { "<leader>e", "<cmd>NvimTreeToggle<CR>", desc = "Toggle file tree" },
+    },
+    config = function()
+      require("nvim-tree").setup({
+        sort = { sorter = "case_sensitive" },
+        view = { width = 30 },
+        renderer = { group_empty = true },
+        filters = { dotfiles = true },
+      })
+    end,
+  },
+}
